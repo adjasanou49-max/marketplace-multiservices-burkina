7a0a94f60f5db82a697237addef87f7c1d4fc43f
adjasanou49-max <adjasanou49@gmail.com>
2026-09-21 22:36:00 +0545

    refactor: use centralized Flutter app entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'app/app.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MarketplaceApp());
-}
-
-class MarketplaceApp extends StatelessWidget {
-  const MarketplaceApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Marketplace Multiservices Burkina',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Marketplace Multiservices Burkina'),
-        ),
-      ),
-    );
-  }
 }
