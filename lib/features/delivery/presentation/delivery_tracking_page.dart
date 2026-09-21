0f963bf373b0a7801ccf122bdb844102aab5004e
adjasanou49-max <adjasanou49@gmail.com>
2026-09-22 03:14:58 +0545

    fix: import Supabase realtime channel type

--- lib/features/delivery/presentation/delivery_tracking_page.dart
+++ lib/features/delivery/presentation/delivery_tracking_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/providers/repository_providers.dart';
