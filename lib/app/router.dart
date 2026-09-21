2e527fc90c0a48f8b311eeeaf9467f113b724103
adjasanou49-max <adjasanou49@gmail.com>
2026-09-21 23:23:49 +0545

    feat: wire seller routes

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -5,15 +5,26 @@ import '../features/cart/presentation/cart_page.dart';
 import '../features/checkout/presentation/checkout_page.dart';
 import '../features/orders/presentation/orders_page.dart';
 import '../features/addresses/presentation/addresses_page.dart';
+import '../features/notifications/presentation/notifications_page.dart';
+import '../features/messaging/presentation/messages_page.dart';
+import '../features/seller/presentation/seller_dashboard_page.dart';
+import '../features/seller/presentation/seller_orders_page.dart';
+import '../features/seller/presentation/seller_order_detail_page.dart';
+import '../features/seller/presentation/seller_stock_page.dart';
+import '../features/seller/presentation/seller_finance_page.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(path: '/', builder: (context, state) => const HomePage()),
-    GoRoute(path: '/search', builder: (context, state) => const SearchPage()),
-    GoRoute(path: '/cart', builder: (context, state) => const CartPage()),
-    GoRoute(path: '/checkout', builder: (context, state) => const CheckoutPage()),
-    GoRoute(path: '/orders', builder: (context, state) => const OrdersPage()),
-    GoRoute(path: '/addresses', builder: (context, state) => const AddressesPage()),
-  ],
-);
+final appRouter=GoRouter(initialLocation:'/',routes:[
+ GoRoute(path:'/',builder:(c,s)=>const HomePage()),
+ GoRoute(path:'/search',builder:(c,s)=>const SearchPage()),
+ GoRoute(path:'/cart',builder:(c,s)=>const CartPage()),
+ GoRoute(path:'/checkout',builder:(c,s)=>const CheckoutPage()),
+ GoRoute(path:'/orders',builder:(c,s)=>const OrdersPage()),
+ GoRoute(path:'/addresses',builder:(c,s)=>const AddressesPage()),
+ GoRoute(path:'/notifications',builder:(c,s)=>const NotificationsPage()),
+ GoRoute(path:'/messages',builder:(c,s)=>const MessagesPage()),
+ GoRoute(path:'/seller',builder:(c,s)=>const SellerDashboardPage()),
+ GoRoute(path:'/seller/orders',builder:(c,s)=>const SellerOrdersPage()),
+ GoRoute(path:'/seller/order/:id',builder:(c,s)=>SellerOrderDetailPage(groupId:s.pathParameters['id']!)),
+ GoRoute(path:'/seller/stock',builder:(c,s)=>SellerStockPage(shopId:s.uri.queryParameters['shopId']??'')),
+ GoRoute(path:'/seller/finance',builder:(c,s)=>const SellerFinancePage()),
+]);
