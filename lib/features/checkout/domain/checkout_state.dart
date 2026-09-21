4a8cb9c51e46edd7d6102bc7be980b1e1727c4ba
adjasanou49-max <adjasanou49@gmail.com>
2026-09-22 01:59:13 +0545

    feat: track delivery quote details in checkout state

--- lib/features/checkout/domain/checkout_state.dart
+++ lib/features/checkout/domain/checkout_state.dart
@@ -1,25 +1,34 @@
+
 class CheckoutState {
   const CheckoutState({
     this.addressId,
     this.deliveryFee = 0,
+    this.deliveryDistanceKm,
+    this.deliveryStopCount = 0,
     this.note,
     this.couponCode,
   });
 
   final String? addressId;
   final num deliveryFee;
+  final num? deliveryDistanceKm;
+  final int deliveryStopCount;
   final String? note;
   final String? couponCode;
 
   CheckoutState copyWith({
     String? addressId,
     num? deliveryFee,
+    num? deliveryDistanceKm,
+    int? deliveryStopCount,
     String? note,
     String? couponCode,
   }) =>
       CheckoutState(
         addressId: addressId ?? this.addressId,
         deliveryFee: deliveryFee ?? this.deliveryFee,
+        deliveryDistanceKm: deliveryDistanceKm ?? this.deliveryDistanceKm,
+        deliveryStopCount: deliveryStopCount ?? this.deliveryStopCount,
         note: note ?? this.note,
         couponCode: couponCode ?? this.couponCode,
       );
