87465ebc893a4186ca2b390fd6138b221a51d2e8
adjasanou49-max <adjasanou49@gmail.com>
2026-09-22 01:33:07 +0545

    feat: register product detail navigation route

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../features/home/presentation/home_page.dart';
+import '../features/products/domain/product.dart';
+import '../features/products/presentation/product_detail_page.dart';
 import '../features/search/presentation/search_page.dart';
 import '../features/cart/presentation/cart_page.dart';
 import '../features/checkout/presentation/checkout_page.dart';
@@ -36,6 +38,7 @@ import '../features/expiry/presentation/expiry_page.dart';
 final appRouter=GoRouter(initialLocation:'/',routes:[
  GoRoute(path:'/',builder:(c,s)=>const HomePage()),
  GoRoute(path:'/search',builder:(c,s)=>const SearchPage()),
+ GoRoute(path:'/product/:id',builder:(c,s){final product=s.extra;return product is Product ? ProductDetailPage(product:product) : const Scaffold(body:Center(child:Text('Produit introuvable')));}),
  GoRoute(path:'/cart',builder:(c,s)=>const CartPage()),
  GoRoute(path:'/checkout',builder:(c,s)=>const CheckoutPage()),
  GoRoute(path:'/refunds',builder:(c,s)=>const RefundsPage()),
