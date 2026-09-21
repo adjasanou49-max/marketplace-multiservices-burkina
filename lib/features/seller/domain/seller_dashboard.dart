6b538b04100d77df2ea742a8955ad3f9cdaf4283
adjasanou49-max <adjasanou49@gmail.com>
2026-09-21 23:07:40 +0545

    feat: create seller dashboard foundation

---
+++ lib/features/seller/domain/seller_dashboard.dart
@@ -0,0 +1,7 @@
+class SellerDashboard {
+  const SellerDashboard({required this.shopCount, required this.productCount, required this.pendingOrders, required this.revenue});
+  final int shopCount;
+  final int productCount;
+  final int pendingOrders;
+  final num revenue;
+}
