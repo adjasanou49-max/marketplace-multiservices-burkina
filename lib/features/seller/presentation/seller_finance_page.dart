0e4792310e75c6c5c07d70f11ef015f83c1633d6
adjasanou49-max <adjasanou49@gmail.com>
2026-09-21 23:34:54 +0545

    feat: link seller finance to payout history

--- lib/features/seller/presentation/seller_finance_page.dart
+++ lib/features/seller/presentation/seller_finance_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/providers/repository_providers.dart';
 import '../data/seller_ledger_repository.dart';
+import 'package:go_router/go_router.dart';
 
 class SellerFinancePage extends ConsumerWidget {
  const SellerFinancePage({super.key});
