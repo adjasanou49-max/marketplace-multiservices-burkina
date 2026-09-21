ce0b582f6f623ad23c8ab02bd5fd8ec2529b861a
adjasanou49-max <adjasanou49@gmail.com>
2026-09-22 01:46:37 +0545

    feat: add coupon code to checkout state

--- lib/features/checkout/domain/checkout_state.dart
+++ lib/features/checkout/domain/checkout_state.dart
@@ -1,9 +1,26 @@
 class CheckoutState {
-  const CheckoutState({this.addressId, this.deliveryFee = 0, this.note});
+  const CheckoutState({
+    this.addressId,
+    this.deliveryFee = 0,
+    this.note,
+    this.couponCode,
+  });
+
   final String? addressId;
   final num deliveryFee;
   final String? note;
-  CheckoutState copyWith({String? addressId, num? deliveryFee, String? note}) => CheckoutState(
-    addressId: addressId ?? this.addressId, deliveryFee: deliveryFee ?? this.deliveryFee, note: note ?? this.note,
-  );
+  final String? couponCode;
+
+  CheckoutState copyWith({
+    String? addressId,
+    num? deliveryFee,
+    String? note,
+    String? couponCode,
+  }) =>
+      CheckoutState(
+        addressId: addressId ?? this.addressId,
+        deliveryFee: deliveryFee ?? this.deliveryFee,
+        note: note ?? this.note,
+        couponCode: couponCode ?? this.couponCode,
+      );
 }
