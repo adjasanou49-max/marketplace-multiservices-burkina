c54f52a562b5c17def5348936fc5a676fbf6d9da
adjasanou49-max <adjasanou49@gmail.com>
2026-09-21 22:43:50 +0545

    feat: enable Riverpod application scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/config/app_bootstrap.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppBootstrap.initialize();
-  runApp(const MarketplaceApp());
+  runApp(const ProviderScope(child: MarketplaceApp()));
 }
