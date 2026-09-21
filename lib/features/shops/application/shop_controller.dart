0de8061a0e52dedf5ce35aafd0d15b20ce7e3c2a
adjasanou49-max <adjasanou49@gmail.com>
2026-09-21 22:43:42 +0545

    feat: add shop_controller.dart

---
+++ lib/features/shops/application/shop_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/providers/repository_providers.dart';
+import '../domain/shop.dart';
+
+final shopsProvider = FutureProvider<List<Shop>>((ref) async {
+  final repository = ref.watch(shopRepositoryProvider);
+  if (repository == null) return const [];
+  return repository.fetchActive();
+});
