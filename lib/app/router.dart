1f1a0e8384be6e7d4274195d6d391073d55417aa
adjasanou49-max <adjasanou49@gmail.com>
2026-09-22 02:09:28 +0545

    fix: route seller home through full seller navigation

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -26,6 +26,8 @@ import '../features/seller/presentation/seller_order_detail_page.dart';
 import '../features/seller/presentation/seller_stock_page.dart';
 import '../features/seller/presentation/seller_finance_page.dart';
 import '../features/seller/presentation/seller_payouts_page.dart';
+import '../features/seller/presentation/seller_navigation.dart';
+import '../features/seller/presentation/seller_tools_page.dart';
 import '../features/promotions/presentation/promotions_page.dart';
 import '../features/follows/presentation/follows_page.dart';
 import '../features/profile/presentation/profile_page.dart';
@@ -124,7 +126,7 @@ final appRouter = GoRouter(
  GoRoute(path:'/addresses',builder:(c,s)=>const AddressesPage()),
  GoRoute(path:'/notifications',builder:(c,s)=>const NotificationsPage()),
  GoRoute(path:'/messages',builder:(c,s)=>const MessagesPage()),
- GoRoute(path:'/seller',builder:(c,s)=>const SellerDashboardPage()),
+ GoRoute(path:'/seller',builder:(c,s)=>const SellerNavigation()),
  GoRoute(path:'/seller/orders',builder:(c,s)=>const SellerOrdersPage()),
  GoRoute(path:'/seller/order/:id',builder:(c,s)=>SellerOrderDetailPage(groupId:s.pathParameters['id']!)),
  GoRoute(path:'/seller/stock',builder:(c,s)=>SellerStockPage(shopId:s.uri.queryParameters['shopId']??'')),
