a54f8aa5b86462c5bce47ef50e537561d33f6f0c
adjasanou49-max <adjasanou49@gmail.com>
2026-09-22 03:21:39 +0545

    fix: correct order details provider import path

--- lib/features/checkout/presentation/order_details_page.dart
+++ lib/features/checkout/presentation/order_details_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../core/providers/repository_providers.dart';
+import '../../../core/providers/repository_providers.dart';
 
 class OrderDetailsPage extends ConsumerStatefulWidget {
   const OrderDetailsPage({required this.orderId, super.key});
